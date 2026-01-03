7fd31799a5ebf983627327cd854c983508e1d202
pEduard03 <podosu.edy@gmail.com>
2026-01-03 22:44:13 +0200

    dsds

--- ShortUrlProject/frontend/src/main.jsx
+++ ShortUrlProject/frontend/src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'  // <--- ACEASTĂ LINIE ESTE OBLIGATORIE!
-
+if (typeof window !== "undefined" && typeof window.SyncedNavigator === "undefined") {
+  window.SyncedNavigator = window.navigator;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
